eece858606a7dbe25d70e1fa625bfa5920e53c54
Ethan Dickson <ethan@coder.com>
2024-07-15 06:58:33 +0000

    gen + fmt

--- integration/user-test/main.tf
+++ integration/user-test/main.tf
@@ -22,11 +22,11 @@ data "coderd_user" "ethan" {
 }
 
 resource "coderd_user" "ethan2" {
-  username = "${data.coderd_user.ethan.username}2"
-  name = "${data.coderd_user.ethan.name}2"
-  email = "${data.coderd_user.ethan.email}.au"
-  login_type = "${data.coderd_user.ethan.login_type}"
-  roles = data.coderd_user.ethan.roles
-  suspended = data.coderd_user.ethan.suspended
+  username   = "${data.coderd_user.ethan.username}2"
+  name       = "${data.coderd_user.ethan.name}2"
+  email      = "${data.coderd_user.ethan.email}.au"
+  login_type = data.coderd_user.ethan.login_type
+  roles      = data.coderd_user.ethan.roles
+  suspended  = data.coderd_user.ethan.suspended
 }
 
