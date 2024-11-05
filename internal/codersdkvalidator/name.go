3a61f81bff603cf3cfc389b569d4df5b33097135
McKayla Washburn <mckayla@hey.com>
2024-11-05 21:49:04 +0000

    oh nice

--- internal/codersdkvalidator/name.go
+++ internal/codersdkvalidator/name.go
@@ -1,47 +1,10 @@
 package codersdkvalidator
 
 import (
-	"context"
-
 	"github.com/coder/coder/v2/codersdk"
-	"github.com/hashicorp/terraform-plugin-framework-validators/helpers/validatordiag"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 )
 
-type nameValidator struct {
-	err error
-}
-
 func Name() validator.String {
-	return nameValidator{}
-}
-
-var _ validator.String = nameValidator{}
-
-func (v nameValidator) ValidateString(ctx context.Context, req validator.StringRequest, resp *validator.StringResponse) {
-	if req.ConfigValue.IsNull() || req.ConfigValue.IsUnknown() {
-		return
-	}
-
-	name := req.ConfigValue.ValueString()
-	if v.err = codersdk.NameValid(name); v.err != nil {
-		resp.Diagnostics.Append(validatordiag.InvalidAttributeValueDiagnostic(
-			req.Path,
-			v.Description(ctx),
-			name,
-		))
-	}
-}
-
-var _ validator.Describer = nameValidator{}
-
-func (v nameValidator) Description(_ context.Context) string {
-	if v.err != nil {
-		return v.err.Error()
-	}
-	return "value must be a valid name"
-}
-
-func (v nameValidator) MarkdownDescription(ctx context.Context) string {
-	return v.Description(ctx)
+	return validatorFromFunc(codersdk.NameValid, "value must be a valid name")
 }
---
+++ internal/codersdkvalidator/validator_from_func.go
@@ -0,0 +1,51 @@
+package codersdkvalidator
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/helpers/validatordiag"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+)
+
+type functionValidator struct {
+	check          func(string) error
+	defaultMessage string
+	err            error
+}
+
+func validatorFromFunc(check func(string) error, defaultMessage string) functionValidator {
+	return functionValidator{
+		check:          check,
+		defaultMessage: defaultMessage,
+	}
+}
+
+var _ validator.String = functionValidator{}
+
+func (v functionValidator) ValidateString(ctx context.Context, req validator.StringRequest, resp *validator.StringResponse) {
+	if req.ConfigValue.IsNull() || req.ConfigValue.IsUnknown() {
+		return
+	}
+
+	name := req.ConfigValue.ValueString()
+	if v.err = v.check(name); v.err != nil {
+		resp.Diagnostics.Append(validatordiag.InvalidAttributeValueDiagnostic(
+			req.Path,
+			v.Description(ctx),
+			name,
+		))
+	}
+}
+
+var _ validator.Describer = functionValidator{}
+
+func (v functionValidator) Description(_ context.Context) string {
+	if v.err != nil {
+		return v.err.Error()
+	}
+	return "value must be a valid name"
+}
+
+func (v functionValidator) MarkdownDescription(ctx context.Context) string {
+	return v.Description(ctx)
+}
